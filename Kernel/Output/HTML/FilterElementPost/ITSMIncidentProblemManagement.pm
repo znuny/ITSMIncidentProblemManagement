e84f5592fdd38197d4969bf5ae4d5ce7f4e29757
Falko Saller <2903042+falkos@users.noreply.github.com>
2024-10-21 11:14:09 +0200

    Added a lookup in case the TicketID is empty, but the TicketNumber is available. (#3)
    * Added a lookup in case the TicketID is empty, but the TicketNumber is there.
    
    * Update Kernel/Output/HTML/FilterElementPost/ITSMIncidentProblemManagement.pm
    
    * Update Kernel/Output/HTML/FilterElementPost/ITSMIncidentProblemManagement.pm
    
    ---------
    
    Co-authored-by: Falko Sperker <fsr@fast.ag>
    Co-authored-by: Denny Korsukéwitz <dennykorsukewitz@gmail.com>

--- Kernel/Output/HTML/FilterElementPost/ITSMIncidentProblemManagement.pm
+++ Kernel/Output/HTML/FilterElementPost/ITSMIncidentProblemManagement.pm
@@ -57,7 +57,23 @@ sub Run {
     if ( $Param{TemplateFile} eq 'AgentTicketZoom' ) {
 
         # get ticket id
-        my $TicketID = $ParamObject->GetParam( Param => 'TicketID' );
+        my $TicketID     = $ParamObject->GetParam( Param => 'TicketID' );
+        my $TicketNumber = $ParamObject->GetParam( Param => 'TicketNumber' );
+
+        # get ticket id in case necessary
+        if ( !$TicketID ) {
+            $TicketID = $TicketObject->TicketIDLookup(
+                TicketNumber => $TicketNumber,
+            );
+        }
+
+        if ( !$TicketID ) {
+            $Kernel::OM->Get('Kernel::System::Log')->Log(
+                Priority => 'error',
+                Message  => "Need TicketID or TicketNumber!",
+            );
+            return;
+        }
 
         # Get ticket attributes.
         my %Ticket = $TicketObject->TicketGet(
